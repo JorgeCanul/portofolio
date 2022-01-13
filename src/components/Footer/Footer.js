c51349f362dec35b5bc5bc4afda6b3ac07193192
JorgeCanul <chivas818@gmail.com>
2022-01-13 12:29:29 -0800

    add Hero, Projects, Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,36 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+      <LinkColumn>
+      <LinkTitle>Call</LinkTitle>
+      <LinkItem href='tel:425-289-8878'>(425)289-8878</LinkItem>
+      </LinkColumn>
+
+      <LinkColumn>
+      <LinkTitle>email</LinkTitle>
+      <LinkItem href='mailto:chivas818@gamil.com'>Email</LinkItem>
+      </LinkColumn>
+      </LinkList>
+    <SocialIconsContainer>
+
+      <SocialContainer>
+        <CompanyContainer>
+          <Slogan>Learning <br />Every day</Slogan>
+        </CompanyContainer>
+     <SocialIcons target="_blank" href='https://github.com/JorgeCanul'>
+        <AiFillGithub size="3rem" />
+     </SocialIcons>
+     <SocialIcons target="_blank" href='https://www.linkedin.com/in/jorge-canul-203199137/'>
+        <AiFillLinkedin size="3rem" />
+     </SocialIcons>
+     <SocialIcons target="_blank" href='https://www.instagram.com/jorgecanul2021/'>
+        <AiFillInstagram size="3rem" />
+     </SocialIcons>
+      </SocialContainer>
+    </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
